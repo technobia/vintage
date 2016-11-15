6b02bf81921d15a497f6b024c0394174005ead3b
ka nguyen <khoaanh2212@gmail.com>
2016-11-15 17:39:06 +0700

    init template

---
+++ wp-content/themes/vintage/footer.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kanguyen
+ * Date: 11/15/16
+ * Time: 5:20 PM
+ */?>
+
+<div id="footer">
+    <h1>FOOTER</h1>
+</div>
+</div>
+</body>
+</html>
---
+++ wp-content/themes/vintage/header.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kanguyen
+ * Date: 11/15/16
+ * Time: 5:18 PM
+ */ ?>
+
+<html>
+<head>
+    <title>Tutorial theme</title>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+</head>
+<body>
+<div id="wrapper">
+    <div id="header">
+        <h1>HEADER</h1>
+    </div>
