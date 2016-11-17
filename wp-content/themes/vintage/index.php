76647e798e419d6189d7638a177bf056667c0aee
Binh <technobia@gmail.com>
2016-11-17 23:25:11 +0700

    testing header

--- wp-content/themes/vintage/index.php
+++ wp-content/themes/vintage/index.php
@@ -1,25 +1,59 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: kanguyen
- * Date: 11/15/16
- * Time: 5:19 PM
- */ ?>
-<?php get_header(); ?>
-<div id="main">
-    <?php get_sidebar(); ?>
-    <div id="content">
-        <h1>Main Area</h1>
-        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-            <h1><?php the_title(); ?></h1>
-            <h4>Posted on <?php the_time('F jS, Y') ?></h4>
-            <p><?php the_content(__('(more...)')); ?></p>
-            <hr> <?php endwhile;
-        else: ?>
-            <p><?php _e('Sorry, no posts matched your criteria.'); ?></p><?php endif; ?>
-    </div>
-    <div class="glyphicon glyphicon-adjust"></div>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Vintage</title>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+</head>
+<body>
+
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">Brand</a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+                <li><a href="#">Link</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Another action</a></li>
+                        <li><a href="#">Something else here</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">Separated link</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">One more separated link</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <form class="navbar-form navbar-left">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Search">
+                </div>
+                <button type="submit" class="btn btn-default">Submit</button>
+            </form>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#">Link</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Another action</a></li>
+                        <li><a href="#">Something else here</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">Separated link</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
+<div id="body" class="container-fluid">
+    <?php wp_register_script( 'custom-script', get_template_directory_uri() . '/js/custom-script.js' ); ?>
 </div>
-<div id="delimiter">
-</div>
-<?php get_footer(); ?>
+
+</body>
+</html>
