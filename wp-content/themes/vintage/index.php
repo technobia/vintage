cc5535481c8c30e6e05f1ad5db128ecf829c32b0
Binh <technobia@gmail.com>
2016-11-18 22:10:19 +0700

    main menu

--- wp-content/themes/vintage/index.php
+++ wp-content/themes/vintage/index.php
@@ -3,6 +3,7 @@
 <head>
     <title>Vintage</title>
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+    <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/scripts/jquery.min.js"></script>
     <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/scripts/bootstrap.min.js"></script>
 </head>
 <body>
@@ -10,6 +11,18 @@
     <a href="#" class="navbar-brand">
         <img src="<?php bloginfo('template_url'); ?>/assets/img/logo-vintage.png">
     </a>
+
+    <div class="navbar-right no-margin">
+        <ul class="main-menu list-inline no-margin">
+            <li class="item"><a href="#">Sản Phẩm <i class="fa fa-angle-down"></i></a></li>
+            <li class="item"><a href="#">Hộp Quà</a></li>
+            <li class="item"><a href="#">Hình Ảnh</a></li>
+            <li class="item"><a href="#">Tin Tức</a></li>
+            <li class="item"><a href="#">Liên hệ</a></li>
+            <li class="divine"></li>
+            <li class="item">Tiếng Việt <i class="fa fa-angle-down"></i></li>
+        </ul>
+    </div>
 </nav>
 </body>
 </html>
