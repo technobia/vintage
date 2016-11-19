71de5b7aa4636948b0e81333d7eec18a34953f45
Binh <technobia@gmail.com>
2016-11-19 22:11:20 +0700

    responsive navbar

--- wp-content/themes/vintage/index.php
+++ wp-content/themes/vintage/index.php
@@ -10,37 +10,46 @@
 </head>
 <body>
 <nav class="navbar navbar-default no-border no-margin">
-    <a href="#" class="navbar-brand">
-        <img src="<?php bloginfo('template_url'); ?>/assets/img/logo-vintage.png">
-    </a>
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a href="#" class="navbar-brand">
+            <img src="<?php bloginfo('template_url'); ?>/assets/img/logo-vintage.png">
+        </a>
+    </div>
 
-    <div class="navbar-right no-margin">
-        <ul class="main-menu list-inline no-margin">
-            <li class="item dropdown">
-                <a href="#">Sản Phẩm <i class="fa fa-angle-down"></i></a>
-                <ul class="list-unstyled dropdown-menu no-margin">
-                    <li><a href="#"><i class="fa fa-angle-right"></i>Chile Casas Patronales</a></li>
-                    <li><a href="#"><i class="fa fa-angle-right"></i>Tây Ban Nha Felix Solis</a></li>
-                    <li><a href="#"><i class="fa fa-angle-right"></i>Ý Travaglini</a></li>
-                    <li><a href="#"><i class="fa fa-angle-right"></i>Pháp Producta</a></li>
-                    <li><a href="#"><i class="fa fa-angle-right"></i>Pháp Sovex Grands</a></li>
-                    <li><a href="#"><i class="fa fa-angle-right"></i>Chile San Marco</a></li>
-                    <li><a href="#"><i class="fa fa-angle-right"></i>Chile Paso Fino</a></li>
-                </ul>
-            </li>
-            <li class="item"><a href="#">Hộp Quà</a></li>
-            <li class="item"><a href="#">Hình Ảnh</a></li>
-            <li class="item"><a href="#">Tin Tức</a></li>
-            <li class="item"><a href="#">Liên hệ</a></li>
-            <li class="divine"></li>
-            <li class="item dropdown">
-                Tiếng Việt <i class="fa fa-angle-down"></i>
-                <ul class="list-unstyled dropdown-menu no-margin" style="min-width:90px;">
-                    <li><a href="#">Tiếng Việt</a></li>
-                    <li><a href="#">English</a></li>
-                </ul>
-            </li>
-        </ul>
+    <div class="collapse navbar-collapse in" id="navbar-collapse">
+        <div class="navbar-right no-margin">
+            <ul class="main-menu list-inline no-margin">
+                <li class="item dropdown">
+                    <a href="#" data-toggle="dropdown">Sản Phẩm <i class="fa fa-angle-down"></i></a>
+                    <ul class="list-unstyled dropdown-menu no-margin">
+                        <li><a href="#"><i class="fa fa-angle-right"></i>Chile Casas Patronales</a></li>
+                        <li><a href="#"><i class="fa fa-angle-right"></i>Tây Ban Nha Felix Solis</a></li>
+                        <li><a href="#"><i class="fa fa-angle-right"></i>Ý Travaglini</a></li>
+                        <li><a href="#"><i class="fa fa-angle-right"></i>Pháp Producta</a></li>
+                        <li><a href="#"><i class="fa fa-angle-right"></i>Pháp Sovex Grands</a></li>
+                        <li><a href="#"><i class="fa fa-angle-right"></i>Chile San Marco</a></li>
+                        <li><a href="#"><i class="fa fa-angle-right"></i>Chile Paso Fino</a></li>
+                    </ul>
+                </li>
+                <li class="item"><a href="#">Hộp Quà</a></li>
+                <li class="item"><a href="#">Hình Ảnh</a></li>
+                <li class="item"><a href="#">Tin Tức</a></li>
+                <li class="item"><a href="#">Liên hệ</a></li>
+                <li class="divine visible-sm"></li>
+                <li class="item dropdown">
+                    <a href="#" data-toggle="dropdown">Tiếng Việt <i class="fa fa-angle-down"></i></a>
+                    <ul class="list-unstyled dropdown-menu no-margin" style="min-width:90px;">
+                        <li><a href="#">Tiếng Việt</a></li>
+                        <li><a href="#">English</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
     </div>
 </nav>
 <div class="main-brands">
