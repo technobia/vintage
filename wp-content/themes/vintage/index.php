4514c1fa344f9447afee4bbe1d59a5696c064273
Binh <technobia@gmail.com>
2016-11-19 10:52:46 +0700

    homepage finish

--- wp-content/themes/vintage/index.php
+++ wp-content/themes/vintage/index.php
@@ -5,6 +5,8 @@
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
     <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/scripts/jquery.min.js"></script>
     <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/scripts/bootstrap.min.js"></script>
+    <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/scripts/jquery.rotate.min.js"></script>
+    <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/scripts/custom.js"></script>
 </head>
 <body>
 <nav class="navbar navbar-default no-border no-margin">
@@ -41,5 +43,34 @@
         </ul>
     </div>
 </nav>
+<div class="main-brands">
+    <div id="brands-slide">
+        <div class="slideMain">
+            <ul class="no-margin slide1">
+                <li><a href="#"><img src="<?php bloginfo('template_url'); ?>/assets/img/logo.jpg"></a></li>
+                <li><a href="#"><img src="<?php bloginfo('template_url'); ?>/assets/img/logo-vintage.png"></a></li>
+                <li><a href="#"><img src="<?php bloginfo('template_url'); ?>/assets/img/logo.jpg"></a></li>
+                <li><a href="#"><img src="<?php bloginfo('template_url'); ?>/assets/img/logo-vintage.png"></a></li>
+                <li><a href="#"><img src="<?php bloginfo('template_url'); ?>/assets/img/logo.jpg"></a></li>
+                <li><a href="#"><img src="<?php bloginfo('template_url'); ?>/assets/img/logo-vintage.png"></a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="address">
+        <h1>MTV IMPORT EXPORT TRADING JOINT STOCK COMPANE</h1>
+        <p>
+            <span>Tel: 08 6660 5068</span>
+            <span class="divine"></span>
+            <span>Fax: 08 3744 2342</span>
+        </p>
+        <p>109/8 Nguyen Binh Khiem Street, DaKao Ward, District 1, HCM</p>
+        <p>
+            <span><i class="fa fa-globe"></i><a href="#">www.vintage.vn</a></span>
+            <span class="divine"></span>
+            <span><i class="fa fa-envelope-o"></i><a href="mailto:info@vintage.vn">info@vintage.vn</a> - <a
+                    href="mailto:mtvwines@yahoo.com">mtvwines@yahoo.com</a></span>
+        </p>
+    </div>
+</div>
 </body>
 </html>
---
+++ wp-content/themes/vintage/scripts/custom.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $("#brands-slide").rotate({
+        speed: 15,
+        width: '1024px'
+    });
+});
