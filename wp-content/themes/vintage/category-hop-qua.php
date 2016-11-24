457e2e7836e62ac284fd8dcfad2a2898672d3bf7
Binh <technobia@gmail.com>
2016-11-24 23:17:40 +0700

    packager

--- wp-content/themes/vintage/category-hop-qua.php
+++ wp-content/themes/vintage/category-hop-qua.php
@@ -6,5 +6,26 @@
  * Time: 11:05 PM
  */
 get_header();
-var_dump($cat);
-get_footer();
+?>
+<div class="container-fluid" id="packager">
+	<div class="container">
+		<div class="item col-xs-12 col-sm-6 col-md-4">
+			<img src="<?php bloginfo('template_url'); ?>/assets/img/thumb-1.jpg">
+		</div>
+		<div class="item col-xs-12 col-sm-6 col-md-4">
+			<img src="<?php bloginfo('template_url'); ?>/assets/img/thumb-1.jpg">
+		</div>
+		<div class="item col-xs-12 col-sm-6 col-md-4">
+			<img src="<?php bloginfo('template_url'); ?>/assets/img/thumb-1.jpg">
+		</div>
+		<div class="item col-xs-12 col-sm-6 col-md-4">
+			<img src="<?php bloginfo('template_url'); ?>/assets/img/thumb-1.jpg">
+		</div>
+		<div class="item col-xs-12 col-sm-6 col-md-4">
+			<img src="<?php bloginfo('template_url'); ?>/assets/img/thumb-1.jpg">
+		</div>
+		<div class="item col-xs-12 col-sm-6 col-md-4">
+			<img src="<?php bloginfo('template_url'); ?>/assets/img/thumb-1.jpg">
+		</div>
+	</div>
+</div>
