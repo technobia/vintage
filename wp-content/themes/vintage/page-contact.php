d1f88fc2583b8c7aa617f6f0fe88b435347ede06
Ka Nguyen <khoaanh2212@gmail.com>
2016-11-28 10:41:59 +0700

    private page for contact

---
+++ wp-content/themes/vintage/page-contact.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kanguyen
+ * Date: 11/28/16
+ * Time: 10:37 AM
+ */
+get_header(); ?>
+
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
+            <?php
+            // Start the loop.
+            while (have_posts()) {
+                the_post();
+                the_content();
+            }
+            ?>
+
+        </main><!-- .site-main -->
+    </div><!-- .content-area -->
+
+<?php get_footer(); ?>
