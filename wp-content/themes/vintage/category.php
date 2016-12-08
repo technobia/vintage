0e3a876386103ffa6f92dab29c40eb4033abee1f
Binh <technobia@gmail.com>
2016-12-08 23:44:40 +0700

    List Product

--- wp-content/themes/vintage/category.php
+++ wp-content/themes/vintage/category.php
@@ -7,4 +7,25 @@
  */
 
 get_header(); ?>
-<div>Danh sach san pham</div>
+<div id="product-list" class="container-responsive">
+	<div class="container">
+		<?php
+		for ($i = 1; $i <= 6; $i++) {
+		?>
+			<div class="col-sm-6">
+				<div class="item-inner">
+					<div class="pic"><img src="<?php bloginfo( 'template_url' ); ?>/assets/img/NequeV.png"></div>
+					<div class="info">
+						<div class="title">Cabernet</div>
+						<div class="description">
+							This Reserva wine is red with violet notes. Its earthy bouquet is complemented by fresh fruit, black cherries, spices and tobacco. On the palate, the wine is smooth and fresh with prunes and black pepper and robust, silky tannins.
+						</div>
+						<div class="price">
+							$10.99
+						</div>
+					</div>
+				</div>
+			</div>
+		<?php } ?>
+	</div>
+</div>
