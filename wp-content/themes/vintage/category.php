f2d87d5076163e33da638c4a8be73ba6a70a168f
Ka Nguyen <khoaanh2212@gmail.com>
2016-12-09 00:33:59 +0700

    integrate data into category overview page

--- wp-content/themes/vintage/category.php
+++ wp-content/themes/vintage/category.php
@@ -7,25 +7,37 @@
  */
 
 get_header(); ?>
+
 <div id="product-list" class="container-responsive">
-	<div class="container">
-		<?php
-		for ($i = 1; $i <= 6; $i++) {
-		?>
-			<div class="col-sm-12 col-md-6">
-				<div class="item-inner">
-					<div class="pic"><img src="<?php bloginfo( 'template_url' ); ?>/assets/img/NequeV.png"></div>
-					<div class="info">
-						<div class="title">Cabernet</div>
-						<div class="description">
-							This Reserva wine is red with violet notes. Its earthy bouquet is complemented by fresh fruit, black cherries, spices and tobacco. On the palate, the wine is smooth and fresh with prunes and black pepper and robust, silky tannins.
-						</div>
-						<div class="price">
-							$10.99
-						</div>
-					</div>
-				</div>
-			</div>
-		<?php } ?>
-	</div>
+    <div class="container">
+        <?php
+        $listCategoryOfPost = get_the_category();
+        foreach ($listCategoryOfPost as $categoryOfPost) {
+            $agrCategory = array('category' => $categoryOfPost->cat_ID);
+            $postsCategory = get_posts($agrCategory);
+            foreach ($postsCategory as $post) {
+                setup_postdata($post);
+                $thumb_id = get_post_thumbnail_id();
+                $thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true);
+                $thumb_url = $thumb_url_array[0]; ?>
+                <div class="col-sm-12 col-md-6">
+                    <div class="item-inner">
+                        <div class="pic"><img src="<?php echo $thumb_url; ?>"></div>
+                        <div class="info">
+                            <div class="title"><?php the_title() ?></div>
+                            <div class="description">
+                                <?php the_excerpt() ?>
+                            </div>
+                            <div class="price">
+                                <?php echo get_post_meta(get_the_ID(), 'price', TRUE);;
+                                ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <?php
+            }
+        } ?>
+    </div>
 </div>
+<?php get_footer(); ?>
