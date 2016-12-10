545f523594a835981c522bd7f7bcbec36f3a1675
binh <technobia@gmail.com>
2016-12-10 14:25:58 +0700

    remove unusing code

--- Makefile
+++
@@ -1,8 +0,0 @@
-.PHONY: build
-build: docker-image
-
-delete:
-	docker rmi -f vintage:latest
-
-docker-image:
-	docker build -t vintage:latest .
--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-make build
-docker-compose -f docker-compose.yml up
-docker cp vintage.sql vintage_mysql_1:/
-docker cp init_db.sh vintage_mysql_1:/
-docker exec vintage_mysql_1 /bin/bash init_db.sh
