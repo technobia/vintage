6872cfe4ff98ae07c0b6a1f5049badb887e3f556
Ka Nguyen <khoaanh2212@gmail.com>
2016-12-12 00:09:25 +0700

    add price into gift box overview page

--- wp-content/themes/vintage/category-hop-qua.php
+++ wp-content/themes/vintage/category-hop-qua.php
@@ -20,13 +20,13 @@ $loop = new WP_Query($args);
             $thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true);
             $thumb_url = $thumb_url_array[0];
             echo '<div class="item col-xs-12 col-sm-6 col-md-4">';
-                echo '<div class="item-inner">';
-                    echo '<img src="' . $thumb_url . '"/>';
-                    echo '<div class="info">';
-                        echo '<h5>'.get_the_title().'</h5>';
-                        echo '<div class="price">Vui lòng gọi</div>';
-                    echo '</div>';
-                echo '</div>';
+            echo '<div class="item-inner">';
+            echo '<img src="' . $thumb_url . '"/>';
+            echo '<div class="info">';
+            echo '<h5>' . get_the_title() . '</h5>';
+            echo '<div class="price">' . get_post_meta(get_the_ID(), 'currency', TRUE) . get_post_meta(get_the_ID(), 'price', TRUE) . '</div>';
+            echo '</div>';
+            echo '</div>';
             echo '</div>';
         endwhile; ?>
     </div>
