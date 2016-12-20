bb428c6d15ff9f1439c09f8b23fe0d10668ea485
binh <technobia@gmail.com>
2016-12-20 23:33:18 +0700

    update picture

--- wp-content/themes/vintage/category-hinh-anh.php
+++ wp-content/themes/vintage/category-hinh-anh.php
@@ -18,10 +18,12 @@ $loop = new WP_Query($args);
 			$thumb_id = get_post_thumbnail_id();
 			$thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true);
 			$thumb_url = $thumb_url_array[0];
-			echo '<div class="item col-xs-12 col-sm-6 col-md-4">';
+            echo '<div class="item col-xs-12 col-sm-6 col-md-4">';
             echo '<div class="item-inner">';
-			echo '<img src="' . $thumb_url . '" data-action="zoom">';
-			echo '</div>';
+            echo '<a href="'.$thumb_url.'" rel="prettyPhoto[gift]">';
+            echo '<img src="'.$thumb_url.'"/>';
+            echo '</a>';
+            echo '</div>';
             echo '</div>';
 		endwhile; ?>
 	</div>
