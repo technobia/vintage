f324bd338546772d9d5152f602a6e3108678f3d6
binh <technobia@gmail.com>
2016-12-21 23:19:24 +0700

    no rotate for special product

--- wp-content/themes/vintage/category.php
+++ wp-content/themes/vintage/category.php
@@ -62,14 +62,19 @@ get_header(); ?>
             'order' => 'ASC',
         );
         $postsCategory = get_posts($agrCategory);
+        $no_rotate = [285, 248, 288];
+
         foreach ($postsCategory as $post) {
             setup_postdata($post);
             $thumb_id = get_post_thumbnail_id();
             $thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true);
-            $thumb_url = $thumb_url_array[0]; ?>
+            $thumb_url = $thumb_url_array[0];
+            $no_rotate_class = '';
+            if (in_array($post->ID, $no_rotate)) { $no_rotate_class = 'no-rotate'; }
+            ?>
             <div class="col-sm-12 col-md-6">
                 <div class="item-inner">
-                    <div class="pic"><img src="<?php echo $thumb_url; ?>"></div>
+                    <div class="pic"><img src="<?php echo $thumb_url; ?>" class="<?=$no_rotate_class?>"></div>
                     <div class="info">
                         <div class="title">
                             <a href="<?php echo esc_url(get_permalink()); ?>">
