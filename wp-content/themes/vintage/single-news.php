4d2bc2abb05dd59ee2b37d009c0996095f32dc9e
binh <technobia@gmail.com>
2016-12-28 21:29:48 +0700

    news content html update

--- wp-content/themes/vintage/single-news.php
+++ wp-content/themes/vintage/single-news.php
@@ -15,24 +15,14 @@ $thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true
 $thumb_url = $thumb_url_array[0];
 $listCategoryOfPost = get_the_category();
 ?>
-<div id="product-detail" class="container-responsive">
+<div id="news-detail" class="container-responsive">
     <div class="product-detail-inner">
         <?php while (have_posts()) {
             the_post(); ?>
             <div class="product-detail">
-                <div class="content">
-                    <div class="description">
-                        <h1 class="title"><?=the_title() ?></h1>
-                            <?=the_content(); ?>
-                    </div>
-                    <div class="image">
-                        <div class="image-inner">
-                            <a href="<?php echo $thumb_url; ?>" rel="prettyPhoto" class="magnifier-thumb-wrapper">
-                                <img src="<?php echo $thumb_url; ?>" id="thumb" class="original zoom">
-                            </a>
-                            <div class="magnifier-preview" id="preview"></div>
-                        </div>
-                    </div>
+                <div class="description">
+                    <h1 class="title"><?=the_title() ?></h1>
+                    <?=the_content(); ?>
                 </div>
             </div>
         <?php } ?>
