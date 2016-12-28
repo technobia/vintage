ca624a85f150442dfd9b245302397e47374726b1
binh <technobia@gmail.com>
2016-12-28 21:35:32 +0700

    overflow news description

--- wp-content/themes/vintage/category-tin-tuc.php
+++ wp-content/themes/vintage/category-tin-tuc.php
@@ -23,7 +23,7 @@ $loop = new WP_Query($args);
             ?>
             <div class="item col-xs-12">
                 <div class="item-left col-xs-12 col-sm-12 col-md-4">
-                    <a href="#">
+                    <a href="<?=esc_url(get_permalink())?>">
                         <img class="item-object" src="<?php echo $thumb_url; ?>">
                     </a>
                 </div>
@@ -35,42 +35,3 @@ $loop = new WP_Query($args);
         <?php } ?>
     </div>
 </div>
-?>
-<!--<div class="container-fluid container-responsive" id="news-list">
-	<div class="container">
-		<div class="item col-xs-12">
-			<div class="item-left col-xs-12 col-sm-12 col-md-4">
-				<a href="#">
-					<img class="item-object" src="<?php /*bloginfo('template_url'); */ ?>/assets/img/gallery.jpg">
-				</a>
-			</div>
-			<div class="item-body col-xs-12 col-sm-12 col-md-8">
-				<h4 class="item-heading"><a href="#">Media heading</a></h4>
-				<p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
-			</div>
-		</div>
-		<div class="item col-xs-12">
-			<div class="item-left col-xs-12 col-sm-12 col-md-4">
-				<a href="#">
-					<img class="item-object" src="<?php /*bloginfo('template_url'); */ ?>/assets/img/gallery.jpg">
-				</a>
-			</div>
-			<div class="item-body col-xs-12 col-sm-12 col-md-8">
-				<h4 class="item-heading"><a href="#">Media heading</a></h4>
-				<p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
-			</div>
-		</div>
-		<div class="item col-xs-12">
-			<div class="item-left col-xs-12 col-sm-12 col-md-4">
-				<a href="#">
-					<img class="item-object" src="<?php /*bloginfo('template_url'); */ ?>/assets/img/gallery.jpg">
-				</a>
-			</div>
-			<div class="item-body col-xs-12 col-sm-12 col-md-8">
-				<h4 class="item-heading"><a href="#">Media heading</a></h4>
-				<p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
-			</div>
-		</div>
-	</div>
-</div>
--->
