87ee273b7098fef5008b84e9bcafce6fd3b6439b
binh <technobia@gmail.com>
2017-01-02 22:06:40 +0700

    implement new pictures

--- wp-content/themes/vintage/category-hinh-anh.php
+++ wp-content/themes/vintage/category-hinh-anh.php
@@ -11,20 +11,47 @@ $args = array('post_type' => 'pictures', 'posts_per_page' => -1);
 $loop = new WP_Query($args);
 
 ?>
-<div class="container-fluid container-responsive giftbox" id="packager">
-	<div class="container">
-		<?php
-		while ($loop->have_posts()) : $loop->the_post();
-			$thumb_id = get_post_thumbnail_id();
-			$thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true);
-			$thumb_url = $thumb_url_array[0];
-            echo '<div class="item col-xs-12 col-sm-6 col-md-4">';
-            echo '<div class="item-inner">';
-            echo '<a href="'.$thumb_url.'" rel="prettyPhoto[gift]">';
-            echo '<img src="'.$thumb_url.'"/>';
-            echo '</a>';
-            echo '</div>';
-            echo '</div>';
-		endwhile; ?>
-	</div>
+<div class="container-fluid container-responsive giftbox" id="pictures">
+    <div id="pictures-carousel" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <?php
+            $thumb_num = 0;
+            while ($loop->have_posts()) : $loop->the_post();
+                $thumb_num++;
+                $thumb_id = get_post_thumbnail_id();
+                $thumb_url = wp_get_attachment_image_src($thumb_id, 'thumbnail', true)[0];
+
+                echo '<li data-target="#pictures-carousel" data-slide-to="'.($thumb_num - 1).'" class="'.($thumb_num == 1 ? 'active' : '').'">';
+                echo '<img src="'.$thumb_url.'">';
+                echo '</li>';
+            endwhile;
+            ?>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+            <?php
+            $image_num = 0;
+            while ($loop->have_posts()) : $loop->the_post();
+                $image_num++;
+                $thumb_id = get_post_thumbnail_id();
+                $image_url = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true)[0];
+                echo '<div class="item '.($image_num == 1 ? 'active' : '').'">';
+                echo '<img src="'.$image_url.'">';
+                echo '</div>';
+            endwhile;
+            ?>
+        </div>
+
+        <!-- Controls -->
+        <a class="left carousel-control" href="#pictures-carousel" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#pictures-carousel" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
 </div>
